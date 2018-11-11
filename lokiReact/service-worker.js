4e0d91196b5b7315ab41b034a2c0bb8961bfc9af
Adam Round <adamround@hotmail.com>
2018-11-10 21:21:50 -0500

    Adding loki locations files

--- lokiReact/service-worker.js
+++ lokiReact/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e58c6bce4e00e56bfb6ca758f814c1b.js"
+  "/Portfolio/lokiReact/precache-manifest.3c9c90c1187668b709b997f1b8eb9d29.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Portfolio/lokiReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
