b64302d5868e3d492ccc18d62fad46f33a215af7
Adam Round <adamround@hotmail.com>
2020-04-22 13:53:57 +0100

    commiting piano app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Key from './components/Key';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    const notes = ["C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6"];
+
+    let keys = [];
+
+    for (const n of notes) {
+      if (n.includes('#')) {
+        keys.push(<div className="blackKeyWrapper">
+        <Key note={n} type="black" />
+      </div>)
+      } else {
+        keys.push(<Key note={n} type="white"/>)
+      }
+    }
+
+    console.log(keys);
+
+    return (
+        <div className="pianoContainer">
+          {keys}
+        </div>
+      );
+}
 }
 
 export default App;
