a1d0e289f320364d817cfd2126f634ff15642440
Adam Round <adamround@hotmail.com>
2020-04-23 20:17:34 +0100

    adding piano build

---
+++ piano/precache-manifest.b71c4b086c77ec530bd1d9ca9cfb2094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b985b8691b2d5535520dce0873340e73",
+    "url": "/Portfolio/piano/index.html"
+  },
+  {
+    "revision": "9418d9358d433a815731",
+    "url": "/Portfolio/piano/static/css/main.17df8a06.chunk.css"
+  },
+  {
+    "revision": "4c21ce6625b100aa56d2",
+    "url": "/Portfolio/piano/static/js/2.a633351c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/piano/static/js/2.a633351c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9418d9358d433a815731",
+    "url": "/Portfolio/piano/static/js/main.cfa4af6d.chunk.js"
+  },
+  {
+    "revision": "57f48fe57ba532f027b6",
+    "url": "/Portfolio/piano/static/js/runtime-main.ef44ce99.js"
+  }
+]);
